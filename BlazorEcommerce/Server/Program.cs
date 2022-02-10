2c9f45c3232905dcd7a7630304dc7fa9729e8ff0
Douglas Willian Dias <douglas.wdias@hotmail.com>
2022-02-10 19:12:35 -0300

    Product Details, Categorys and Seeding more products

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,7 +1,8 @@
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Server.Data;
-using Microsoft.AspNetCore.ResponseCompression;
+global using BlazorEcommerce.Server.Services.ProductService;
+global using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,8 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
