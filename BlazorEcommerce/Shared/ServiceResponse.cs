aa06c33e854fee6d4aa312d16b2301afca18b42b
Douglas Willian Dias <douglas.wdias@hotmail.com>
2022-03-04 09:53:34 -0300

    Search

--- BlazorEcommerce/Shared/ServiceResponse.cs
+++ BlazorEcommerce/Shared/ServiceResponse.cs
@@ -11,5 +11,6 @@ namespace BlazorEcommerce.Shared
         public T? Data { get; set; }
         public bool Success { get; set; } = true;
         public string Message { get; set; } = string.Empty;
+
     }
 }
