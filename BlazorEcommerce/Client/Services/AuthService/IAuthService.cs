5d376bd356bc6253b2487099609d9a25e9d62879
Douglas <douglas.wdias@hotmail.com>
2022-10-09 10:09:20 -0300

    Add Login, Logout and User Authentication.

---
+++ BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Client.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<int>> Register(UserRegister register);
+        Task<ServiceResponse<string>> Login(UserLogin request);
+    }
+}
--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -3,8 +3,9 @@ global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Server.Data;
 global using BlazorEcommerce.Server.Services.ProductService;
 global using BlazorEcommerce.Server.Services.CategoryService;
-global using Microsoft.AspNetCore.ResponseCompression;
-using BlazorEcommerce.Server.Services.CartService;
+global using BlazorEcommerce.Server.Services.CartService;
+global using BlazorEcommerce.Server.Services.AuthService;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
